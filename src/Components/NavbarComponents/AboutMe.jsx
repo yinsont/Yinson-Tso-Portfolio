9a4fe96ad44b5903d5ff57caaab88b291556789a
Yinson Tso <yinson.tso@gmail.com>
2023-06-14 23:12:53 -0400

    Updated About Me Page

--- src/Components/NavbarComponents/AboutMe.jsx
+++ src/Components/NavbarComponents/AboutMe.jsx
@@ -4,9 +4,28 @@ import Navbar from "../Navbar"
 
 function AboutMe(){
     return (
-        <div>
-            <h3>About Me</h3>
-            <p></p>
+        <div className="Projects">
+            <div className="project-container">
+                <h1>Background</h1>
+                <p>Hello, my name is Yinson and I'm an Asian-American, first child to an immigrant family. Born and raised in NYC.</p>
+            </div >
+            <div className="project-container">
+                <h1>Hobbies</h1>
+                <p>I love games and playing in general, sports, video games, card games, thinking games, you name it. I love going for walks and just relaxing, listening to music.</p>
+            </div>
+            <div className="project-container">
+                <h1>Dreams and Aspirations</h1>
+                <p>My goal is to somehow make my parents happy and give them a peace of mind so that they can relax. But if we are talking personal, then it's to make a game. I've always loved video games and wish to create my own one day.</p>
+            </div>
+            <div className="project-container">
+                <h1>Programming</h1>
+                <p>I left highschool with the ability to use Arduino. I am currently a student at The Flatiron School where I'm learning how to become a Software Engineer. So far, I can confidantly say that I have a firm grasp on Javascript, React, Python, SQL, and HTML.</p>
+            </div>
+            <div className="project-container">
+                <h1>Current Plans?</h1>
+                <p>If we're referring to this portfolio, this was made in a rush. The current iteration is not what it's meant to be. It will be updated near the end of my bootcamp.</p>
+                <p>I have some more project plans in mind but I'd also like to enter the work force as soon as I can! We shall see how it goes</p>
+            </div>
         </div>
     )
 }
--- src/Components/ProfilePicture.jsx
+++ src/Components/ProfilePicture.jsx
@@ -1,9 +1,10 @@
 // import image from '../Images/noice.PNG'
 import noice from '../Assets/noice.PNG'
+import PFP from '../Assets/PFP.jpg'
 function ProfilePicture(){
     return (
         // <div className="PFP">
-            <img src = {noice} alt = "Penguin" id = "PFP"/>
+            <img src = {PFP} alt = "Penguin" id = "PFP"/>
         // </div>
     )
 }
